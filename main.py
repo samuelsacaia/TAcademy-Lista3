c5caef4f082d2d0abfefe9511d1679d49ab5ac09
samuelsacaia <samuelsacaia24@gmail.com>
2022-10-06 10:34:46 -0300

    first

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 
 
-from src.application.domain import converter_em_class
+from src.application.domain import converter_em_class, pegar_saldo
 from src.application.input import pega_arquivo
 
 
 if __name__=='__main__':
     arquivo_transactions = pega_arquivo()
     lista_trasanctions = converter_em_class(arquivo_transactions)
+    lista_saldo = pegar_saldo(lista_trasanctions)
     
